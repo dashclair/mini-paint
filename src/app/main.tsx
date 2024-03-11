1bbf9684ca9cd13cb62c0cce1f6271fbed36eaf5
Dasha <darya.gapeeva.01@gmail.com>
2024-03-11 16:17:56 +0300

    refactor: change main imports

---
+++ src/app/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes/router.tsx';
+import { MainLayout } from '../shared/ui/index.ts';
+
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <MainLayout>
+      <RouterProvider router={router}></RouterProvider>
+    </MainLayout>
+  </React.StrictMode>,
+);
