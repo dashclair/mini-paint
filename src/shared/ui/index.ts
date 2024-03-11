2220f8708ea8e1445ca6ae0010961c15b37f6e7d
Dasha <darya.gapeeva.01@gmail.com>
2024-03-11 16:26:08 +0300

    feat: create public api for shared ui

---
+++ src/shared/ui/index.ts
@@ -0,0 +1,4 @@
+export { CustomButton } from './CustomButton';
+export { AuthLayout } from './AuthLayout';
+export { AuthFormContainer } from './AuthFormContainer';
+export { MainLayout } from './MainLayout';
