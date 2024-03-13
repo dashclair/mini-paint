6eee13cd94cda29de35d54ecd7659e6c646c82da
Dasha <darya.gapeeva.01@gmail.com>
2024-03-13 20:16:28 +0300

    feat: add AppRouter

--- src/app/routes/router.tsx
+++ src/app/routes/router.tsx
@@ -1,9 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { ROUTE_NAMES } from '../../shared/router/routeNames';
 import { LoginPage, RegistrationPage } from '../../pages';
+import { MainPage } from '../../pages/MainPage';
 
 export const router = createBrowserRouter([
-  { path: ROUTE_NAMES.HOME, element: '' },
+  {
+    path: ROUTE_NAMES.HOME,
+    element: <MainPage />,
+  },
   { path: ROUTE_NAMES.LOGIN, element: <LoginPage /> },
   { path: ROUTE_NAMES.SIGNUP, element: <RegistrationPage /> },
 ]);
