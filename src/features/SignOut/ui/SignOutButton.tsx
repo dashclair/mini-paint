b86cb627e83cf9e11547969f2d9d2325cc723224
Dasha <darya.gapeeva.01@gmail.com>
2024-03-16 17:11:14 +0300

    feat: add signout feature

--- src/features/SignOut/ui/SignOutButton.tsx
+++ src/features/SignOut/ui/SignOutButton.tsx
@@ -1,13 +1,13 @@
-import { getAuth, signOut } from 'firebase/auth';
 import { CustomButton } from '../../../shared/ui';
 import { useAppDispatch } from '../../../shared/model/hooks';
+import { signOut } from 'firebase/auth';
+import { auth } from '../../../app/config/firbase';
 import { setUnAuth } from '../../../entities/User/model/userSlice';
 
 export const SignOutButton = () => {
   const dispatch = useAppDispatch();
 
   const handleSignOut = () => {
-    const auth = getAuth();
     signOut(auth)
       .then(() => {
         console.log('sign out');
