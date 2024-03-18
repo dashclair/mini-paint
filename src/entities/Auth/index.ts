7c26cd9995ae9ff7344c7e10e21869f5ce23b6c4
Dasha <darya.gapeeva.01@gmail.com>
2024-03-18 17:56:29 +0300

    feat: add publick api for auth entity

---
+++ src/entities/Auth/index.ts
@@ -0,0 +1,4 @@
+export { AuthProvider } from './model/AuthProvider';
+export { useAuth } from './model/useAuth';
+export { passwordValidationRules, emailValidationRules } from './helpers/validation';
+export { signIn, signUp } from './helpers/authServices';
