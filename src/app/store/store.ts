18ed7c68a8cd6f686d67456a7963cf7154b12bc9
Dasha <darya.gapeeva.01@gmail.com>
2024-03-19 01:19:48 +0300

    refactor: edit store imports

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import userReducer from '../../entities/User/model/userSlice';
+import { userReducer } from '../../entities/User';
 
 export const store = configureStore({
   reducer: {
