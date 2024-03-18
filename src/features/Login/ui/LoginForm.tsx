19c5f8335ea67f3834886c883894230bdefbd5fc
Dasha <darya.gapeeva.01@gmail.com>
2024-03-18 17:57:49 +0300

    feat: add form.item from andt and set auth validation

--- src/features/Login/ui/LoginForm.tsx
+++ src/features/Login/ui/LoginForm.tsx
@@ -1,14 +1,18 @@
 import { AuthFormContainer, CustomButton } from '../../../shared/ui';
 import { LoginInputProps } from '../types/LoginInput.types';
-import { Input } from 'antd';
+import { Form, Input } from 'antd';
 import { Controller, useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import { ROUTE_NAMES } from '../../../shared/router/routeNames';
+import { emailValidationRules, passwordValidationRules, signIn } from '../../../entities/Auth';
 import styles from './LoginForm.module.scss';
-import { signIn } from '../../../entities/Auth/helpers/authServices';
 
 export const LoginForm = () => {
-  const { control, handleSubmit } = useForm<LoginInputProps>();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginInputProps>();
   const navigate = useNavigate();
 
   const handleSubmitForm = async ({ email, password }: LoginInputProps) => {
@@ -27,24 +31,36 @@ export const LoginForm = () => {
   return (
     <AuthFormContainer>
       <h1 className={styles.title}>Sign in</h1>
-      <form className={styles.form} onSubmit={handleSubmit(handleSubmitForm)}>
+      <Form className={styles.form} onFinish={handleSubmit(handleSubmitForm)}>
         <Controller
           name="email"
           control={control}
+          rules={emailValidationRules}
           render={({ field }) => (
-            <Input size="large" placeholder="Email" className={styles.input} {...field} />
+            <Form.Item
+              validateStatus={errors.email ? 'error' : ''}
+              help={errors.email && errors.email.message}
+            >
+              <Input size="large" placeholder="Email" className={styles.input} {...field} />
+            </Form.Item>
           )}
         />
         <Controller
           name="password"
           control={control}
+          rules={passwordValidationRules}
           render={({ field }) => (
-            <Input.Password
-              size="large"
-              placeholder="Password"
-              className={styles.input}
-              {...field}
-            />
+            <Form.Item
+              validateStatus={errors.password ? 'error' : ''}
+              help={errors.password && errors.password.message}
+            >
+              <Input.Password
+                size="large"
+                placeholder="Password"
+                className={styles.input}
+                {...field}
+              />
+            </Form.Item>
           )}
         />
         <div className={styles.btnContainer}>
@@ -62,7 +78,7 @@ export const LoginForm = () => {
             htmlType="submit"
           />
         </div>
-      </form>
+      </Form>
     </AuthFormContainer>
   );
 };
--- src/features/Registration/ui/RegistrationForm.tsx
+++ src/features/Registration/ui/RegistrationForm.tsx
@@ -1,14 +1,20 @@
-import { Input } from 'antd';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Form, Input } from 'antd';
 import { AuthFormContainer, CustomButton } from '../../../shared/ui';
 import { Controller, useForm } from 'react-hook-form';
 import { RegistrationInputProps } from '../types/RegistrationInput.types';
 import { useNavigate } from 'react-router-dom';
 import { ROUTE_NAMES } from '../../../shared/router/routeNames';
 import styles from './RegistrationForm.module.scss';
-import { signUp } from '../../../entities/Auth/helpers/authServices';
+import { emailValidationRules, passwordValidationRules, signUp } from '../../../entities/Auth';
 
 export const RegistrationForm = () => {
-  const { control, handleSubmit } = useForm<RegistrationInputProps>();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm<RegistrationInputProps>();
   const navigate = useNavigate();
 
   const handleLogin = () => {
@@ -24,39 +30,67 @@ export const RegistrationForm = () => {
     }
   };
 
+  const confirmPasswordValidationRules = {
+    required: 'Confirm password is required',
+    validate: {
+      matchesPreviousPassword: (value: string) => {
+        const password = watch('password');
+        return value === password || 'Passwords do not match';
+      },
+    },
+  };
+
   return (
     <AuthFormContainer>
       <h1 className={styles.title}>Registration</h1>
-      <form className={styles.form} onSubmit={handleSubmit(handleSubmitRegistration)}>
+      <Form className={styles.form} onFinish={handleSubmit(handleSubmitRegistration)}>
         <Controller
           name="email"
           control={control}
+          rules={emailValidationRules}
           render={({ field }) => (
-            <Input size="large" placeholder="Email" className={styles.input} {...field} />
+            <Form.Item
+              validateStatus={errors.email ? 'error' : ''}
+              help={errors.email && errors.email.message}
+            >
+              <Input size="large" placeholder="Email" className={styles.input} {...field} />
+            </Form.Item>
           )}
         />
         <Controller
           name="password"
           control={control}
+          rules={passwordValidationRules}
           render={({ field }) => (
-            <Input.Password
-              size="large"
-              placeholder="Password"
-              className={styles.input}
-              {...field}
-            />
+            <Form.Item
+              validateStatus={errors.password ? 'error' : ''}
+              help={errors.password && errors.password.message}
+            >
+              <Input.Password
+                size="large"
+                placeholder="Password"
+                className={styles.input}
+                {...field}
+              />
+            </Form.Item>
           )}
         />
         <Controller
           name="confirmPassword"
           control={control}
+          rules={confirmPasswordValidationRules as any}
           render={({ field }) => (
-            <Input.Password
-              size="large"
-              placeholder="Confirm password"
-              className={styles.input}
-              {...field}
-            />
+            <Form.Item
+              validateStatus={errors.confirmPassword ? 'error' : ''}
+              help={errors.confirmPassword && errors.confirmPassword.message}
+            >
+              <Input.Password
+                size="large"
+                placeholder="Confirm password"
+                className={styles.input}
+                {...field}
+              />
+            </Form.Item>
           )}
         />
         <div className={styles.btnContainer}>
@@ -74,7 +108,7 @@ export const RegistrationForm = () => {
             htmlType="submit"
           />
         </div>
-      </form>
+      </Form>
     </AuthFormContainer>
   );
 };
