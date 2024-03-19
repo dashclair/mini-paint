0ce358f40d6d9e530d66bbeb24ec93bbeb85254f
Dasha <darya.gapeeva.01@gmail.com>
2024-03-19 17:58:04 +0300

    refactor: rename useAuth to useUser and place to User slice

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -3,3 +3,4 @@ import userReducer from './model/userSlice';
 export { userReducer };
 export { selectUser } from './model/userSlice';
 export { setUser, setUnAuth } from './model/userSlice';
+export { useUser } from './model/useUser';
---
+++ src/entities/User/model/useUser.ts
@@ -0,0 +1,32 @@
+import { auth } from '../../../app/config/firbase';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useCallback, useEffect } from 'react';
+import { selectUser, setUnAuth, setUser } from '../../User';
+import { useAppDispatch, useAppSelector } from '../../../shared/model/hooks';
+
+export const useUser = () => {
+  const dispatch = useAppDispatch();
+  const authData = useAppSelector(selectUser);
+
+  const handleOnAuthChanged = useCallback(() => {
+    return onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const uid = user?.uid;
+        const email = user?.email;
+        dispatch(setUser({ uid, email }));
+      } else {
+        dispatch(setUnAuth());
+      }
+    });
+  }, [dispatch]);
+
+  useEffect(() => {
+    handleOnAuthChanged();
+  }, [handleOnAuthChanged]);
+
+  const userId = authData.userData?.id;
+  const isAuth = authData.isAuth;
+  const isLoading = authData.pending;
+
+  return { userId, isAuth, isLoading };
+};
