70dbd09c42d2e726597e26329935d85647e3df6d
Dasha <darya.gapeeva.01@gmail.com>
2024-03-19 17:56:43 +0300

    refactor: remove authServices and add new feature segments

--- src/entities/Auth/index.ts
+++
@@ -1,3 +0,0 @@
-export { useAuth } from './model/useAuth';
-export { passwordValidationRules, emailValidationRules } from './helpers/validation';
-export { signIn, signUp, handleSignOut } from './helpers/authServices';
