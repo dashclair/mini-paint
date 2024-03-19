d172b141e72ba1eb232594a37af7ab3071b615e8
Dasha <darya.gapeeva.01@gmail.com>
2024-03-19 15:38:35 +0300

    refactor: add exit icon with IconComponent and edit imports

--- src/features/SignOut/ui/SignOutButton.tsx
+++ src/features/SignOut/ui/SignOutButton.tsx
@@ -1,12 +1,12 @@
-import { CustomButton } from '../../../shared/ui';
-import { handleSignOut } from '../../../entities/Auth';
+import { CustomButton } from 'shared/ui';
+import { handleSignOut } from 'entities/Auth';
 import styles from './SignOutButton.module.scss';
-import ExitButton from 'assets/exit.svg?react';
+import { IconComponent } from 'shared/ui';
 
 export const SignOutButton = () => {
   return (
     <CustomButton className={styles.buttonContainer} type="link" onClick={handleSignOut}>
-      {<ExitButton />}
+      {<IconComponent iconName="exit" />}
     </CustomButton>
   );
 };
