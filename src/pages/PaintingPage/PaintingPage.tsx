0ad7dfd0c538b92923d71b14462c41f45b1b635f
Dasha <darya.gapeeva.01@gmail.com>
2024-03-20 18:18:52 +0300

    refactor: add tools and canvas to paintiong page

--- src/pages/PaintingPage/PaintingPage.tsx
+++ src/pages/PaintingPage/PaintingPage.tsx
@@ -1,12 +1,15 @@
 import { Canvas } from 'features/Paint';
 import styles from './PaintingPage.module.scss';
 import { Tools } from 'entities/Tools';
+import { useState } from 'react';
 
 export const PaintingPage = () => {
+  const [tool, setTool] = useState<string | null>(null);
+  const [width, setWidth] = useState<string>('3');
   return (
     <div className={styles.container}>
-      <Tools />
-      <Canvas />
+      <Tools width={width} setWidth={setWidth} setTool={setTool} />
+      <Canvas width={width} tool={tool} />
     </div>
   );
 };
