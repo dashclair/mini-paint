9b5c23a8f14b827d39dd072955265cac1055a9d3
Dasha <darya.gapeeva.01@gmail.com>
2024-03-20 18:05:18 +0300

    feat: refactor iconsMap and icons

--- src/shared/ui/Icon/lib/iconsMap.ts
+++ src/shared/ui/Icon/lib/iconsMap.ts
@@ -1,5 +1,13 @@
 import ExitIcon from '../assets/exit.svg?react';
+import PenIcon from '../assets/pencil-svgrepo-com.svg?react';
+import CircleIcon from '../assets/circle-svgrepo-com.svg?react';
+import LineIcon from '../assets/line-svgrepo-com.svg?react';
+import RectangleIcon from '../assets/rectangle-svgrepo-com.svg?react';
 
 export const iconsMap = {
   exit: { component: ExitIcon },
+  pen: { component: PenIcon },
+  circle: { component: CircleIcon },
+  line: { component: LineIcon },
+  rectangle: { component: RectangleIcon },
 };
