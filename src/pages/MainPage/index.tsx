111004c3e58508f1e53d48b24bbdda24eb430c6f
Dasha <darya.gapeeva.01@gmail.com>
2024-03-22 19:49:58 +0300

    refactor: edit main page styles

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,3 +1,24 @@
+import { useEffect } from 'react';
+import { FeedLayout } from 'widgets/Feed';
+import { useAppDispatch } from 'shared/model/hooks';
+import { picsAsyncThunk } from 'entities/Post/model/fetchThunk';
+
+import styles from './MainPage.module.scss';
+
 export const MainPage = () => {
-  return <div>Main page</div>;
+  const dispatch = useAppDispatch();
+  const handleFatchingPosts = async () => {
+    await dispatch(picsAsyncThunk());
+  };
+
+  useEffect(() => {
+    handleFatchingPosts();
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      Main page
+      <FeedLayout />
+    </div>
+  );
 };
