26d7a623bbb4c05dec87332814492993186f7d04
Dasha <darya.gapeeva.01@gmail.com>
2024-03-22 19:39:09 +0300

    refactor: change postSlice properties and reducers

--- src/entities/Post/model/postsSlice.ts
+++ src/entities/Post/model/postsSlice.ts
@@ -1,26 +1,37 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { RootState } from 'app/store/store';
+import { picsAsyncThunk } from './fetchThunk';
 
 interface IPosts {
-  uid: string;
-  email: string;
-  link: string;
+  imageURL: string;
+  userEmail: string;
 }
 
 interface IPostsInitialState {
-  posts: IPosts[] | null;
+  posts: IPosts[] | null | undefined;
+  isLoading: boolean;
 }
 
 const initialState: IPostsInitialState = {
-  posts: [],
+  isLoading: true,
+  posts: null,
 };
 
 export const postsSlice = createSlice({
   name: 'posts',
   initialState,
-  reducers: {
-    setPosts: (state, { payload }) => {
-      const posts = state.posts;
-      posts?.push(payload);
-    },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(picsAsyncThunk.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(picsAsyncThunk.fulfilled, (state, { payload }) => {
+      state.isLoading = false;
+      state.posts = payload;
+    });
   },
 });
+
+export const selectPosts = (state: RootState) => state.posts;
+
+export default postsSlice.reducer;
