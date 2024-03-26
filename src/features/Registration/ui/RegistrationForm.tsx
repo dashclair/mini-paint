fa549a1a92fd2d5900474e90f5aacd5d4a2b53d8
Dasha <darya.gapeeva.01@gmail.com>
2024-03-26 18:37:21 +0300

    refactor: update signUp logic with useMutation hook

--- src/features/Registration/ui/RegistrationForm.tsx
+++ src/features/Registration/ui/RegistrationForm.tsx
@@ -1,6 +1,6 @@
 import { Controller, useForm } from 'react-hook-form';
 import { Navigate, useNavigate } from 'react-router-dom';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import { Form, Input } from 'antd';
 import { selectUser } from 'entities/User';
 import { emailValidationRules, passwordValidationRules } from 'shared/helpers';
@@ -8,9 +8,11 @@ import { RegistrationInputProps } from '../types/RegistrationInput.types';
 import { ROUTE_NAMES } from 'shared/router/routeNames';
 import { AuthFormContainer, CustomButton } from 'shared/ui';
 import { useAppSelector } from 'shared/model/hooks';
+import { useMutation } from 'shared/model/useMutation';
+import { UserCredential, createUserWithEmailAndPassword } from '@firebase/auth';
+import { auth } from 'app/config/firbase';
 import 'react-toastify/dist/ReactToastify.css';
 import styles from './RegistrationForm.module.scss';
-import { signUp } from '../lib/signUp';
 
 export const RegistrationForm = () => {
   const {
@@ -20,13 +22,15 @@ export const RegistrationForm = () => {
     watch,
   } = useForm<RegistrationInputProps>();
 
+  const { mutate, isLoading } = useMutation<RegistrationInputProps, UserCredential>(
+    ({ email, password }) => createUserWithEmailAndPassword(auth, email, password),
+  );
+
   const navigate = useNavigate();
   const user = useAppSelector(selectUser);
-
   const isAuth = user.isAuth;
 
   if (isAuth) {
-    console.log('navigate');
     return <Navigate to={ROUTE_NAMES.HOME} />;
   }
 
@@ -35,12 +39,7 @@ export const RegistrationForm = () => {
   };
 
   const handleSubmitRegistration = async (data: RegistrationInputProps) => {
-    try {
-      await signUp(data);
-      navigate(ROUTE_NAMES.HOME);
-    } catch (error) {
-      toast.error('This email is used');
-    }
+    await mutate(data);
   };
 
   const confirmPasswordValidationRules = {
@@ -108,10 +107,17 @@ export const RegistrationForm = () => {
           )}
         />
         <div className={styles.btnContainer}>
-          <CustomButton type="default" className={styles.btn} size="large" onClick={handleLogin}>
+          <CustomButton
+            disabled={isLoading}
+            type="default"
+            className={styles.btn}
+            size="large"
+            onClick={handleLogin}
+          >
             Sign in
           </CustomButton>
           <CustomButton
+            disabled={isLoading}
             className={`${styles.btn} ${styles.btn_primary}`}
             size="large"
             htmlType="submit"
