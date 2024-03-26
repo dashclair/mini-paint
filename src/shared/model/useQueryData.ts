b115a7f74ee6ae7bfe8c9e8c36af188dd90aee82
Dasha <darya.gapeeva.01@gmail.com>
2024-03-26 12:08:07 +0300

    feat: create useQueryData hook

---
+++ src/shared/model/useQueryData.ts
@@ -0,0 +1,37 @@
+import { useCallback, useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+
+type QueryFunction<T> = () => Promise<T>;
+
+type UseQueryReturn<T> = {
+  data: T | null;
+  isError?: boolean;
+  isLoading?: boolean;
+};
+
+export const useQueryData = <T>(queryFunction: QueryFunction<T>): UseQueryReturn<T> => {
+  const [isLoading, setIsLoading] = useState<boolean>();
+  const [data, setData] = useState<T | null>(null);
+  const [error, setError] = useState('');
+
+  const fetchData = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      setError('');
+      const data = await queryFunction();
+      setData(data);
+
+      return data;
+    } catch (error) {
+      toast.error('Something went wrong');
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  return { data, isError: Boolean(error), isLoading };
+};
--- src/widgets/Feed/ui/FeedLayout.tsx
+++ src/widgets/Feed/ui/FeedLayout.tsx
@@ -1,23 +1,33 @@
-import { useAppSelector } from 'shared/model/hooks';
-import { selectPosts } from 'entities/Post';
-import { Card } from 'antd';
+import { Card, Spin } from 'antd';
 import Meta from 'antd/es/card/Meta';
 import styles from './FeedLayout.module.scss';
+import { QueryDocumentSnapshot, collection, getDocs, query } from 'firebase/firestore';
+import { db } from 'app/config/firbase';
+import { useQueryData } from 'shared/model/useQueryData';
 
 export const FeedLayout = () => {
-  const postsSelector = useAppSelector(selectPosts);
-  const posts = postsSelector.posts;
+  const q = query(collection(db, 'images'));
+  const querySnapshot = () => {
+    return getDocs(q);
+  };
+
+  const { data, isLoading } = useQueryData(querySnapshot);
+
+  const posts = data?.docs.map((doc: QueryDocumentSnapshot) => {
+    const picsInfo = doc.data();
+    const imageURL = picsInfo.imageURL;
+    const userEmail = picsInfo.userEmail;
+
+    return { imageURL, userEmail };
+  });
+
+  console.log('loading', isLoading);
 
   return (
     <div className={styles.feedWidgenContainer}>
       {posts?.map((post) => {
         return (
-          <Card
-            key={post.imageURL}
-            hoverable
-            style={{ width: 240 }}
-            cover={<img alt="example" src={post.imageURL} />}
-          >
+          <Card key={post.imageURL} hoverable style={{ width: 240 }} cover={<Spin />}>
             <Meta description={post.userEmail} />
           </Card>
         );
