bdf5ac52859a57857b33ee3c88dd10e935747d99
Dasha <darya.gapeeva.01@gmail.com>
2024-03-27 17:10:51 +0300

    feat: add skeletons when posts undefined

--- src/widgets/Feed/ui/FeedLayout.tsx
+++ src/widgets/Feed/ui/FeedLayout.tsx
@@ -8,21 +8,27 @@ import { useGetCards } from '../model/useGetCards';
 export const FeedLayout = () => {
   const { posts, isLoading, handleSetImageLoad, imageIsLoad } = useGetCards();
 
+  const skeletonsArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
   return (
     <LayoutLoader isLoading={isLoading}>
       <div className={styles.feedWidgenContainer}>
-        {posts?.map((post) => {
-          return (
-            <Card
-              key={post.imageURL}
-              hoverable
-              cover={<img onLoad={handleSetImageLoad} alt="example" src={post.imageURL} />}
-            >
-              <Skeleton loading={imageIsLoad} paragraph />
-              <Meta description={post.userEmail} />
-            </Card>
-          );
-        })}
+        {posts
+          ? posts.map((post) => {
+              return (
+                <Card
+                  key={post.imageURL}
+                  hoverable
+                  cover={<img onLoad={handleSetImageLoad} alt="example" src={post.imageURL} />}
+                >
+                  <Skeleton loading={imageIsLoad} paragraph />
+                  <Meta description={post.userEmail} />
+                </Card>
+              );
+            })
+          : skeletonsArray.map((skeleton) => {
+              return <Skeleton key={skeleton} loading={imageIsLoad} active />;
+            })}
       </div>
     </LayoutLoader>
   );
