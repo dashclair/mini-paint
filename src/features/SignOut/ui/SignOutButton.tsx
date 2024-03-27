c122638206c5a2d4681e859dc8b2e3b130d861ab
Dasha <darya.gapeeva.01@gmail.com>
2024-03-27 16:58:59 +0300

    feat: add modal dialog for exit

--- src/features/SignOut/ui/SignOutButton.tsx
+++ src/features/SignOut/ui/SignOutButton.tsx
@@ -2,11 +2,27 @@ import { CustomButton } from 'shared/ui';
 import { IconComponent } from 'shared/ui';
 import styles from './SignOutButton.module.scss';
 import { signOutUser } from '../lib/signOutUser';
+import { Modal } from 'antd';
+import { useState } from 'react';
 
 export const SignOutButton = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   return (
-    <CustomButton type="link" onClick={signOutUser}>
-      {<IconComponent iconName="exit" className={styles.exitIcon} />}
-    </CustomButton>
+    <>
+      <CustomButton type="link" onClick={showModal}>
+        {<IconComponent iconName="exit" className={styles.exitIcon} />}
+      </CustomButton>
+      <Modal title="Exit" open={isModalOpen} onOk={signOutUser} onCancel={handleCancel}>
+        <p>Are you sure you want to leave the page?</p>
+      </Modal>
+    </>
   );
 };
