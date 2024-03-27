68dcce425af6c90e902bc400bfc2ca7cb67c9ca6
Dasha <darya.gapeeva.01@gmail.com>
2024-03-27 22:17:41 +0300

    refacotr: add function to usePaintingPage hook

--- src/pages/PaintingPage/lib/usePaintinPage.ts
+++ src/pages/PaintingPage/lib/usePaintinPage.ts
@@ -1,19 +1,59 @@
+import { db, storage } from 'app/config/firbase';
 import { selectUser } from 'entities/User';
+import { addDoc, collection } from 'firebase/firestore';
+import { getDownloadURL, ref, uploadString } from 'firebase/storage';
 import { useState } from 'react';
-// import { v4 as uuidv4 } from 'uuid';
+import { toast } from 'react-toastify';
+import { v4 as uuidv4 } from 'uuid';
 import { useAppSelector } from 'shared/model/hooks';
-// import { useMutation } from 'shared/model/useMutation';
-// import { UploadResult, getDownloadURL, ref, uploadString } from 'firebase/storage';
-// import { db, storage } from 'app/config/firbase';
-// import { addDoc, collection } from 'firebase/firestore';
-// import { toast } from 'react-toastify';
 
 export const usePaintingPage = () => {
   const [tool, setTool] = useState<string | null>(null);
   const [width, setWidth] = useState<string>('3');
   const [color, setColor] = useState('#aabbcc');
   const user = useAppSelector(selectUser);
+  const [isLoading, setIsLoading] = useState(false);
   const userEmail = user.userData!.email;
 
-  return { tool, setTool, width, setWidth, setColor, color, userEmail };
+  const handleUploadFile = async (pic: string) => {
+    const picId = uuidv4();
+
+    const storageRef = ref(storage, `images/${picId}`);
+    const uploadImage = uploadString(storageRef, pic, 'base64', {
+      contentType: 'image/png',
+    });
+
+    try {
+      setIsLoading(true);
+      const snapshot = await uploadImage;
+      const downloadURL = await getDownloadURL(snapshot.ref);
+
+      const uploading = await addDoc(collection(db, 'images'), {
+        imageURL: downloadURL,
+        date: new Date(),
+        userEmail: userEmail,
+      });
+
+      console.log('uploading image in add doc', uploading);
+      toast.success('Successfully uploaded');
+    } catch (error) {
+      toast.error('Something went wrong during the upload process');
+      setIsLoading(false);
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return {
+    tool,
+    setTool,
+    width,
+    setWidth,
+    setColor,
+    color,
+    userEmail,
+    handleUploadFile,
+    isLoading,
+  };
 };
