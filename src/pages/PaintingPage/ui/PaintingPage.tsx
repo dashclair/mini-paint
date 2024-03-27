65b55e7a151c6a91cc49f0078bcd92ca4f62c183
Dasha <darya.gapeeva.01@gmail.com>
2024-03-27 22:19:43 +0300

    fix: replace logic to hook form painting page

--- src/pages/PaintingPage/ui/PaintingPage.tsx
+++ src/pages/PaintingPage/ui/PaintingPage.tsx
@@ -1,45 +1,12 @@
 import { Canvas } from 'features/Paint';
 import { Tools } from 'entities/Tools';
-import { ToastContainer, toast } from 'react-toastify';
-import styles from './PaintingPage.module.scss';
+import { ToastContainer } from 'react-toastify';
 import { usePaintingPage } from '../lib/usePaintinPage';
-import { v4 as uuidv4 } from 'uuid';
-import { getDownloadURL, ref, uploadString } from 'firebase/storage';
-import { db, storage } from 'app/config/firbase';
-import { addDoc, collection } from 'firebase/firestore';
+import styles from './PaintingPage.module.scss';
 
 export const PaintingPage = () => {
-  const { tool, setTool, width, setWidth, setColor, color, userEmail } = usePaintingPage();
-
-  // const { mutate, isLoading} = useMutation<string, UploadResult>((pic) =>
-  //   uploadString(storageRef, pic, 'base64', {
-  //     contentType: 'image/png',
-  //   }),
-  // );
-
-  const handleUploadFile = (pic: string) => {
-    const picId = uuidv4();
-
-    const storageRef = ref(storage, `images/${picId}`);
-    const uploadImage = uploadString(storageRef, pic, 'base64', {
-      contentType: 'image/png',
-    });
-
-    uploadImage.then((snapshot) => {
-      getDownloadURL(snapshot.ref).then((downloadURL) => {
-        try {
-          addDoc(collection(db, 'images'), {
-            imageURL: downloadURL,
-            date: new Date(),
-            userEmail: userEmail,
-          });
-          toast.success('Successfully uploaded');
-        } catch (error) {
-          console.log(error);
-        }
-      });
-    });
-  };
+  const { tool, setTool, width, setWidth, setColor, color, isLoading, handleUploadFile } =
+    usePaintingPage();
 
   return (
     <div className={styles.container}>
@@ -51,7 +18,13 @@ export const PaintingPage = () => {
         color={color}
         setColor={setColor}
       />
-      <Canvas color={color} width={width} tool={tool} handleUploadImage={handleUploadFile} />
+      <Canvas
+        isLoading={isLoading}
+        color={color}
+        width={width}
+        tool={tool}
+        handleUploadImage={handleUploadFile}
+      />
     </div>
   );
 };
