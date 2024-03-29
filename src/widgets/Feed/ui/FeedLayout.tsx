2fb0669966332b497c556faca64ba58b9fada5ce
Dasha <101951052+dashclair@users.noreply.github.com>
2024-03-29 14:30:01 +0300

    Feature/ filtering pictures (#8)
    * feat: add filter logic
    
    * feat: add logic for filtering with selecting multiple emails

--- src/widgets/Feed/ui/FeedLayout.tsx
+++ src/widgets/Feed/ui/FeedLayout.tsx
@@ -1,34 +1,48 @@
-import { Card } from 'antd';
-import Meta from 'antd/es/card/Meta';
-import styles from './FeedLayout.module.scss';
 import { LayoutLoader } from 'shared/ui';
 import { Skeleton } from 'antd';
 import { useGetCards } from '../model/useGetCards';
+import { Card, Select } from 'antd';
+import Meta from 'antd/es/card/Meta';
+import styles from './FeedLayout.module.scss';
 
 export const FeedLayout = () => {
-  const { posts, isLoading, handleSetImageLoad, imageIsLoad } = useGetCards();
-
-  const skeletonsArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+  const {
+    posts,
+    imageIsLoad,
+    isLoading,
+    emails,
+    handleSetImageLoad,
+    handleOpen,
+    handleSelectUser,
+    handleDeselect,
+    shouldShowPost,
+  } = useGetCards();
 
   return (
     <LayoutLoader isLoading={isLoading}>
+      <Select
+        mode="tags"
+        style={{ width: '100%' }}
+        options={emails}
+        onSelect={handleSelectUser}
+        onDeselect={handleDeselect}
+        onDropdownVisibleChange={handleOpen}
+      />
       <div className={styles.feedWidgenContainer}>
         {posts
-          ? posts.map((post) => {
-              return (
-                <Card
-                  key={post.imageURL}
-                  hoverable
-                  cover={<img onLoad={handleSetImageLoad} alt="example" src={post.imageURL} />}
-                >
-                  <Skeleton loading={imageIsLoad} paragraph />
-                  <Meta description={post.userEmail} />
-                </Card>
-              );
-            })
-          : skeletonsArray.map((skeleton) => {
-              return <Skeleton key={skeleton} loading={imageIsLoad} active />;
-            })}
+          ?.filter((post) => shouldShowPost(post))
+          .map((post) => {
+            return (
+              <Card
+                key={post.imageURL}
+                hoverable
+                cover={<img onLoad={handleSetImageLoad} alt="example" src={post.imageURL} />}
+              >
+                <Skeleton loading={imageIsLoad} paragraph />
+                <Meta description={post.userEmail} />
+              </Card>
+            );
+          })}
       </div>
     </LayoutLoader>
   );
