18332608ab01649c32ce8ee6e366d31b2d9c5126
Dasha <darya.gapeeva.01@gmail.com>
2024-04-02 16:33:27 +0300

    fix: update iconMap imports

--- src/shared/ui/Icon/lib/iconsMap.ts
+++ src/shared/ui/Icon/lib/iconsMap.ts
@@ -3,8 +3,8 @@ import PenIcon from '../assets/pencil-svgrepo-com.svg?react';
 import CircleIcon from '../assets/circle-svgrepo-com.svg?react';
 import LineIcon from '../assets/line-svgrepo-com.svg?react';
 import RectangleIcon from '../assets/rectangle-svgrepo-com.svg?react';
-import LightMode from '../assets/light-on-sun-black-outline.svg?react';
-import DarkMode from '../assets/moon-6679.svg?react';
+import LightMode from '../assets/light-mode-svgrepo-com.svg?react';
+import DarkMode from '../assets/dark-mode-night-moon-svgrepo-com.svg?react';
 
 export const iconsMap = {
   exit: { component: ExitIcon },
