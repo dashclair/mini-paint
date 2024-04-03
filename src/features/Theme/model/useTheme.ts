8f4c8228ee2777aca9d415923b5ea6baaaea364d
Dasha <darya.gapeeva.01@gmail.com>
2024-04-03 18:52:20 +0300

    feat: add local storage for keeping theme

--- src/features/Theme/model/useTheme.ts
+++ src/features/Theme/model/useTheme.ts
@@ -1,16 +1,41 @@
 import { useEffect, useState } from 'react';
 
+const getInitialTheme = () => {
+  const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+  const initialTheme = localStorage.getItem('theme');
+  if (!initialTheme) {
+    console.log('prefered dark', prefersDark);
+    return prefersDark ? 'dark' : 'light';
+  }
+  console.log('initial theme', initialTheme);
+  return initialTheme;
+};
+
 export const useTheme = () => {
-  const [theme, setTheme] = useState('light');
-  const nextTheme = theme === 'light' ? 'dark' : 'light';
+  const [theme, setTheme] = useState(getInitialTheme());
+
+  const getThemeFromLocalStorage = () => {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+      setTheme(savedTheme);
+    }
+  };
 
   const handleSetTheme = () => {
-    setTheme(nextTheme);
+    setTheme((prevTheme) => {
+      const newTheme = prevTheme === 'light' ? 'dark' : 'light';
+      localStorage.setItem('theme', newTheme);
+      return newTheme;
+    });
   };
 
   useEffect(() => {
+    getThemeFromLocalStorage();
     document.body.dataset.theme = theme;
   }, [theme]);
+
+  console.log('theme', theme);
 
   return { theme, handleSetTheme };
 };
